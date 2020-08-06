661e66391a6fd5e3e906dd5694862e7f648ec50b
Oleg Panagushin <opanagushin@gmail.com>
2020-08-06 17:18:42 +0300

    Updates

---
+++ precache-manifest.99ed4ef42e5a24f44ea75e1db99c2343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5f3a5bcfb9a0850eda0c7a036f217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b6ffb81e8bb6292559",
+    "url": "/static/css/main.5f446994.chunk.css"
+  },
+  {
+    "revision": "b890598b01d62adde850",
+    "url": "/static/js/2.45c8d908.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.45c8d908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b6ffb81e8bb6292559",
+    "url": "/static/js/main.d88388ad.chunk.js"
+  },
+  {
+    "revision": "a5ea60d6c88a0d34a412",
+    "url": "/static/js/runtime-main.466ccbfb.js"
+  }
+]);
